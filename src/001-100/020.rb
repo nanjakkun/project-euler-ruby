5486aa81af96980c2e426513ad98f412cc3a5307
nanjakkun <nanjakkun@gmail.com>
2019-09-11 04:16:55 +0900

    Solve problem 20

---
+++ src/001-100/020.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Factorial digit sum
+
+# Problem 20
+
+# n! means n × (n − 1) × ... × 3 × 2 × 1
+
+# For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+# and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+# Find the sum of the digits in the number 100!
+
+factorial = ->(n) {(1..n).inject(1) {|acc, i| acc * i}}
+puts factorial.call(100).to_s.split('').map(&:to_i).sum
